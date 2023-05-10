711973c7a5d50ea10f052efd6548f7915d83a701
IvanRizzo28 <58737356+IvanRizzo28@users.noreply.github.com>
2023-05-10 15:31:13 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('title')
+    Home
+@endsection
+
+@section('main')
+    <h1>main</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <h1 class="text-center">@yield('title')</h1>
+</header>
